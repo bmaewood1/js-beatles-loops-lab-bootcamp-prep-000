6ca353d715ab72fb44a5e063f021061f16d58c48
Bailey Wood <woodbmae@gmail.com>
2019-08-06 00:06:22 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // add solution here
+
+function theBeatlesPlay(musicians, instruments){
+  var empty = [];
+  for (var i=0; i < musicians.length; i++) {
+    empty.push(musicians[i] + " plays " + instruments[i]);
+  }
+  return empty;
+}
+
+function johnLennonFacts() {
+  var facts = ["He was the last Beatle to learn to drive", "He was never a vegetarian", "He was a choir boy and boy scout", "He hated the sound of his own voice"];
+  var array = [];
+  var i = 0;
+  while (array.length < facts.length) {
+    array.push(facts[i] + "!!!");
+    i++;
+  }
+  return array;
+}
+
+
+function iLoveTheBeatles(num){
+  var beatle = [];
+  do {
+    beatle.push("I love the Beatles!");
+    num++;
+  } while (num < 15);
+  return beatle;
+}
+
--- test/index-test.js
+++ test/index-test.js
@@ -33,3 +33,4 @@ describe('Beatles Loops', function() {
   });
 
 });
+
